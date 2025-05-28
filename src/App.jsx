e1a89092117c789475993b5e808081f86564522d
Jacquelyn Carmichael <93010142+JVCarmich0959@users.noreply.github.com>
2025-05-28 18:04:36 -0400

    Update about section text

--- src/App.jsx
+++ src/App.jsx
@@ -32,9 +32,9 @@ const App = () => (
           <About
             headshot="/p1.png"
             bio={[
-              'Melissa Michaels is an author, reader, and aspiring novelist. Her love of writing grew out of her passion for reading, her tastes leaning towards Urban Fantasy.',
-              'She is the author of <i>Ravens Transformation</i>, an Urban Fantasy short story featured in Abaculus III, a collection of international tales of science fiction, fantasy, and horror.',
-              'Melissa is a proud member of the North Carolina Writers Network',
+              'Melissa is an author, reader, and novelist. Melissa’s love of writing grew out of her passion for reading, with her tastes leaning towards urban fantasy.',
+              'She is the Author of <i>Raven’s Transformation</i>, an Urban Fantasy short story featured in Abaculus III, a collection of international tales of science fiction, fantasy, and horror.',
+              'Besides writing, she enjoys gaming, hiking, spending time with her family, and her German Shepard - Caesar.',
             ]}
             blurb="More stories and updates coming soon."
           />
