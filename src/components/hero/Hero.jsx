a245b2d3ca5ecec3d1a5a13eebba421505ee1cc6
Jacquelyn Carmichael <93010142+JVCarmich0959@users.noreply.github.com>
2025-05-29 01:18:53 +0000

    Tweaks

--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -44,7 +44,7 @@ export default function Hero() {
             </Suspense>
           </Canvas>
           <div className="hImg">
-            <img src="/Raven.gif" alt="Hero background" loading="lazy" />
+            <img src="/Hero_bg.png" alt="Hero background" loading="lazy" />
           </div>
           <div className="hero-slogan">
             {slogan.map((word, i) => (
