fa1b5e3661e6825a129220824865e543c2029ff6
Jacquelyn Carmichael <jvictoriacarmichael@gmail.com>
2026-06-09 20:41:19 -0400

    Updates

--- assets/react-three-fiber.esm-RnjoMy30.js
+++ assets/react-three-fiber.esm-RnjoMy30.js
@@ -1,4 +1,4 @@
-import{r as Ye,g as fx,j as Dn}from"./index-Br-YBQFt.js";/**
+import{r as Ye,g as fx,j as Dn}from"./index-CWUYP2MY.js";/**
  * @license
  * Copyright 2010-2024 Three.js Authors
  * SPDX-License-Identifier: MIT
---
+++ assets/use-reduced-motion-DtdavqOh.js
@@ -0,0 +1,1 @@
+import{r as o}from"./index-CWUYP2MY.js";import{h as r,i as t,p as s}from"./index-RF0-oCdM.js";function i(){!r.current&&t();const[e]=o.useState(s.current);return e}export{i as u};
