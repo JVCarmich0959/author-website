f34640332f6ac2918bad9c2891203e3d038f71f3
Jacquelyn Carmichael <jvictoriacarmichael@gmail.com>
2026-06-17 17:41:36 -0400

    Updates

--- assets/extends-BfjpREfY.js
+++ assets/extends-BfjpREfY.js
@@ -1,4 +1,4 @@
-import{r as Ye,g as dx,j as Dn}from"./index-DjUlxYjY.js";/**
+import{r as Ye,g as dx,j as Dn}from"./index-BuGzmIfU.js";/**
  * @license
  * Copyright 2010-2024 Three.js Authors
  * SPDX-License-Identifier: MIT
